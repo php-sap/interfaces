3041d10ba705f30e00e0350569c40c2414786a65
Gregor <gregor-j@users.noreply.github.com>
2019-10-15 14:20:53 +0200

    Add exception for missing table elements

---
+++ src/exceptions/ITableElementMissingException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace phpsap\interfaces\exceptions;
+
+/**
+ * Interface ITableElementMissingException
+ *
+ * A table element required by the API is not in the table.
+ *
+ * @package phpsap\interfaces\exceptions
+ * @author  Gregor J.
+ * @license MIT
+ */
+interface ITableElementMissingException extends ISapException
+{
+}
