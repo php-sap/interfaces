665c43f321f2fbe509a4e9776adf6771edc4f170
Gregor <gregor-j@users.noreply.github.com>
2019-12-02 16:29:38 +0100

    remove IConfiguration::getValidConfigKeys()

--- src/Config/IConfiguration.php
+++ src/Config/IConfiguration.php
@@ -14,13 +14,6 @@ namespace phpsap\interfaces\Config;
 interface IConfiguration extends \JsonSerializable
 {
     /**
-     * Get an associative array of all valid configuration keys (as keys of that
-     * array) and whether they are mandatory (as values of that array).
-     * @return array
-     */
-    public static function getValidConfigKeys();
-
-    /**
      * Decode a JSON encoded configuration and return the correct configuration
      * class (A or B) depending on the values set in the configuration.
      * @param string $config JSON encoded configuration.
